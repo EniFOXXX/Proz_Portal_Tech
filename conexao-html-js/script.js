bbd3bd641374a60a9fddd65dceb34e95d694f4a1
Enivaldo_Souza <enif16@gmail.com>
2022-12-13 21:21:05 -0300

    Aula JS 13-12-22

---
+++ conexao-html-js/script.js
@@ -0,0 +1,48 @@
+console.log("Tudo certo")
+let post01 = document.getElementById("post01");
+console.log(post01);
+let postAutor = document.getElementsByClassName("post-autor");
+console.log(postAutor)
+
+let post02 = document.getElementById("post02");
+console.log(post02);
+
+let formulario = document.getElementById("formulario");
+console.log(formulario);
+
+
+let posts = document.getElementsByClassName("post");
+console.log(posts)
+
+let data = document.getElementsByClassName("post-data");
+console.log(data)
+
+let texto = document.getElementsByClassName("post-texto");
+console.log(texto)
+
+let redes = document.getElementsByClassName("lista_redes");
+console.log(redes)
+
+let textPost02 = document.querySelector("#post02 .post-texto");
+console.log(textPost02)
+
+let listRedes = document.querySelector("footer .lista_redes a");
+console.log(listRedes)
+
+let links = document.querySelector("#posts .post-texto a");
+console.log(links)
+
+let negrito = document.querySelector("#posts .post-texto strong");
+console.log(negrito)
+
+let nome = document.querySelectorAll("#formulario .forml #nome");
+console.log(nome)
+
+let listRedes1 = document.querySelector("footer .lista_redes");
+console.log(listRedes1)
+
+let nav = document.querySelectorAll("header .elementos_nav a");
+console.log(nav)
+
+let autorData = document.querySelectorAll("#posts .post .post-autor , .post-data");
+console.log(autorData)
